cb38966146a2f34e5d9b618f4a1528c0b8cacbed
Geilenberg Michelle <michelle.geilenberg@rwth-aachen.de>
2021-02-14 13:41:55 +0100

    implement first version bottom and top bar menu with NavigationUI

--- app/src/main/java/org/bemi/wanikanisrsapp/MainActivity.kt
+++ app/src/main/java/org/bemi/wanikanisrsapp/MainActivity.kt
@@ -2,11 +2,20 @@ package org.bemi.wanikanisrsapp
 
 
 import android.os.Bundle
+import android.os.Handler
+import android.view.Menu
+import android.view.MenuItem
 import android.view.View
+import android.widget.Toast
+import androidx.core.os.postDelayed
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.NavigationUI
 import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import org.bemi.wanikanisrsapp.databinding.ActivityMainBinding
 
@@ -17,6 +26,8 @@ import org.bemi.wanikanisrsapp.databinding.ActivityMainBinding
 class MainActivity : AppCompatActivity() {
 
     private lateinit var navController: NavController
+    private lateinit var bottomNavView: BottomNavigationView
+    private lateinit var appBarConfiguration: AppBarConfiguration
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -24,13 +35,33 @@ class MainActivity : AppCompatActivity() {
         //val binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(R.layout.activity_main)
 
-        // Get the navigation host fragment from this Activity
-        val navHostFragment = supportFragmentManager
-            .findFragmentById(R.id.nav_host_fragment) as NavHostFragment
-        // Instantiate the navController using the NavHostFragment
-        navController = navHostFragment.navController
-        // Make sure actions in the ActionBar get propagated to the NavController
-        setupActionBarWithNavController(navController)
+        setUpViews()
+    }
+
+    // inflate toolbar menu items for top app bar
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.top_app_bar, menu)
+
+        return true
+    }
+
+    // on Click Listener for Toolbar items
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            // navigate to global settings Fragment
+            R.id.settingsFragment -> {
+                navController.navigate(R.id.settingsFragment)
+
+                return true
+            }
+            //  Otherwise, do nothing and use the core event handling
+
+            // when clauses require that all possible paths be accounted for explicitly,
+            //  for instance both the true and false cases if the value is a Boolean,
+            //  or an else to catch all unhandled cases.
+            else -> super.onOptionsItemSelected(item)
+        }
+
     }
 
     /**
@@ -39,4 +70,63 @@ class MainActivity : AppCompatActivity() {
     override fun onSupportNavigateUp(): Boolean {
         return navController.navigateUp() || super.onSupportNavigateUp()
     }
+
+    fun setUpViews() {
+        // Get the navigation host fragment from this Activity
+        val navHostFragment = supportFragmentManager
+            .findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        // Instantiate the navController using the NavHostFragment
+        navController = navHostFragment.navController
+
+        // get bottom navigation menu form Activity
+        bottomNavView = findViewById(R.id.bottom_nav_view) as BottomNavigationView
+        // setup bottom navigation with Navigation Controller
+        NavigationUI.setupWithNavController(bottomNavView, navHostFragment.navController)
+
+        // define single top-level destination
+        // appBarConfiguration = AppBarConfiguration(navHostFragment.navController.graph)
+        // define multiple top-level destinations
+        appBarConfiguration = AppBarConfiguration(setOf(R.id.dashboardFragment, R.id.profileFragment))
+        setupActionBarWithNavController(navHostFragment.navController, appBarConfiguration)
+
+        //findViewById<Toolbar>(R.id.top_app_bar).setupWithNavController(navController, appBarConfiguration)
+    }
+
+    // set Bottom Navigation bar Visible
+    fun showBottomNavigation()
+    {
+        bottomNavView.visibility = View.VISIBLE
+    }
+
+    // hide bottom navigation bar from view
+    fun hideBottomNavigation()
+    {
+        bottomNavView.visibility = View.GONE
+    }
+
+    private var backPressedOnce = false
+
+    // leave application after two back presses in short succession
+    override fun onBackPressed() {
+        if (navController.graph.startDestination == navController.currentDestination?.id)
+        {
+            if (backPressedOnce)
+            {
+                super.onBackPressed()
+                return
+            }
+
+            // popup exit message after first back press
+            backPressedOnce = true
+            Toast.makeText(this, "Press BACK again to exit", Toast.LENGTH_SHORT).show()
+            // reset first back press after two seconds without succeeding action
+            Handler().postDelayed(2000) {
+                backPressedOnce = false
+            }
+        }
+        else {
+            super.onBackPressed()
+        }
+    }
+
 }
