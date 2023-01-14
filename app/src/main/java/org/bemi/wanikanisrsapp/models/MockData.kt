0a0bc3fe69b611289a9a4f446bc431b416534b3f
MichelleGeilenberg <michelle.geilenberg@rwth-aachen.de>
2023-01-14 18:10:52 +0100

    feat: Add Mock data and data model for User Data.

---
+++ app/src/main/java/org/bemi/wanikanisrsapp/models/MockData.kt
@@ -0,0 +1,17 @@
+package org.bemi.wanikanisrsapp.models
+
+import models.User
+
+class MockData {
+
+    val mockUser = User(
+        "never",
+        "123456789",
+        5,
+        User.Preferences(0, false, 0, "", false, false),
+        "myUrl",
+        "yesterday",
+        User.Subscription(true, 100, "anyday", "lifetime"),
+        "Chelly"
+    )
+}
