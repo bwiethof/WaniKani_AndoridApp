8fe36de18e66ba675b319e9a000906df631c2d7c
bene <benedikt.wiethof@rwth-aachen.de>
2023-04-29 12:28:33 +0200

    refactor(api): Remove unused functions

--- WaniKaniAPI/src/main/kotlin/client/WaniKaniClient.kt
+++ WaniKaniAPI/src/main/kotlin/client/WaniKaniClient.kt
@@ -14,12 +14,8 @@ import io.ktor.serialization.kotlinx.json.json
 import kotlinx.serialization.json.Json
 import model.Assignment
 import model.Collection
-import model.Report
-import model.Resource
-import model.Review
 import queries.QueryBuilder
 import queries.QueryBuilderBase
-import kotlin.reflect.KClass
 
 private const val apiRevisionKey = "Wanikani-Revision"
 
@@ -83,13 +79,18 @@ class WaniKaniClient(
             )
         )
 
+    suspend fun getReviews(initializer: QueryBuilder.() -> Unit): model.simple.Collection<Assignment> =
+        model.simple.Collection.fromWanikani(
+            getCollection(
+                getBuilder(
+                    queries.Resource.Reviews,
+                    initializer
+                )
+            )
+        )
 
-    suspend fun getReviews(initializer: QueryBuilder.() -> Unit): Collection<Review> {
-        return getCollection(getBuilder(queries.Resource.Reviews, initializer))
-    }
 
     private suspend inline fun <reified T> getCollection(builder: QueryBuilderBase): Collection<T> {
-
         if (!isCollectionQuery(builder)) throw IllegalArgumentException("Collections request does not allow specific Id")
         return get(builder)
     }
@@ -103,50 +104,4 @@ class WaniKaniClient(
             ?: throw IllegalArgumentException("Resource type needs to be specified!")
 
 
-    // Keeping existing fun keep existing functions
-    private fun getBuilder(initializer: QueryBuilder.() -> Unit) =
-        QueryBuilder().apply(initializer).build()
-            ?: throw IllegalArgumentException("Resource type needs to be specified!")
-
-
-    // TODO make interfaces more generic or simplify queries
-
-
-    private suspend fun <T : Any> getCollection(
-        initializer: QueryBuilder.() -> Unit, c: KClass<T>
-    ): Collection<T> {
-        val builder = getBuilder(initializer)
-
-        if (!isCollectionQuery(builder)) throw IllegalArgumentException("Collections request does not allow specific Id")
-
-        return get(builder)
-    }
-
-    private suspend fun <T> getResource(initializer: QueryBuilder.() -> Unit): Resource<T> {
-        val builder = getBuilder(initializer)
-
-        if (isCollectionQuery(builder)) throw IllegalArgumentException("Specific resource requires id specification")
-
-        return get(builder)
-    }
-
-
-    private suspend fun <T> getReport(initializer: QueryBuilder.() -> Unit): Report<T> {
-        val builder = getBuilder(initializer)
-
-        return get(builder)
-    }
-
-    // Marking this as private since it doesn't work
-    // get ist a reified fun -> function call needs to be either directly or from a reified function call
-    // Possible solutions:
-    // make "get" public
-    private suspend fun <T> getSimpleResource(initializer: QueryBuilder.() -> Unit): model.simple.Resource<T> =
-        model.simple.Resource.fromWanikani(getResource(initializer))
-
-//    private suspend fun <T : Any> getSimpleCollection(initializer: QueryBuilder.() -> Unit): model.simple.Collection<T> =
-//        model.simple.Collection.fromWanikani(getCollection(initializer))
-
-    private suspend fun <T> getSimpleReport(initializer: QueryBuilder.() -> Unit): model.simple.Report<T> =
-        model.simple.Report.fromWanikani(getReport(initializer))
 }
